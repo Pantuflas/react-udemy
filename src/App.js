de17b42eaf6575dedf31d5238280872f8f6444d5
Pantufla <antoniojss94@gmail.com>
2019-12-28 16:54:44 -0600

    Publish Module 2 branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Person from './components/Person';
-import UserContainer from './components/UserContainer.js/UserContainer';
+
 
 
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1>REACT UDEMY COURSE</h1>
-      <UserContainer />
+      <Person />
 
     </div>
   );
